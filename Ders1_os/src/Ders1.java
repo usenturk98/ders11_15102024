5c26d166e74c35d3a2fc06d2f9ae1d8ca9f5b56b
aidata <aidata@10.121.3.83>
2024-10-15 09:23:18 +0300

    first

---
+++ Ders1_os/src/Ders1.java
@@ -0,0 +1,11 @@
+
+public class Ders1 {
+	int a=5;
+	public static void main(String[] args) {
+	
+		System.out.print(" Merhaba ==JAVA== "+" Hoşgeldiniz"+"\n");
+		System.out.println("merhaba");
+
+	}
+
+}
